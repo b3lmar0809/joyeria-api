2c5cc0aa05b3a7f7642605ec1e3263b5e75f59bb
belmar <mati.belmar0625@gmail.com>
2026-02-13 19:57:37 -0300

    Primer commit

---
+++ src/test/java/com/joyeria/joyeria_api/JoyeriaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joyeria.joyeria_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoyeriaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
