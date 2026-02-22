e82ea96e6b9d698abae810b21372a75bc2dc9fdf
belmar <mati.belmar0625@gmail.com>
2026-02-22 15:39:03 -0300

    User service

---
+++ src/main/java/com/joyeria/joyeria_api/service/UserService.java
@@ -0,0 +1,63 @@
+package com.joyeria.joyeria_api.service;
+
+import com.joyeria.joyeria_api.exception.DuplicateResourceException;
+import com.joyeria.joyeria_api.exception.ResourceNotFoundException;
+import com.joyeria.joyeria_api.model.Role;
+import com.joyeria.joyeria_api.model.User;
+import com.joyeria.joyeria_api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * UserService class
+ *
+ * @Version: 1.0.0 - 22 feb. 2026
+ * @Author: Matias Belmar - mati.belmar0625@gmail.com
+ * @Since: 1.0.0 2026/02/22
+ */
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + email));
+    }
+
+    //registra un nuevo usuario
+    public User registerUser(User user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new DuplicateResourceException("User", "email", user.getEmail());
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        if (user.getRole() == null) {
+            user.setRole(Role.CUSTOMER);
+        }
+
+        user.setActive(true);
+
+        return  userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public User getUserById(Long id){
+        return userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User", id));
+    }
+
+    @Transactional(readOnly = true)
+    public User getUserByEmail(String emil) {
+        return userRepository.findByEmail(emil).orElseThrow(() -> new ResourceNotFoundException("user", "email", emil));
+    }
+}
